409bab45378e107560be2143b03393ebb96a3bc2
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-02-19 17:25:10 +0000

    ✨ add `ansi` pkg

---
+++ ansi/ansi.go
@@ -0,0 +1,34 @@
+// Package ansi provides some helpers to use ANSI coloring
+// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors:~:text=Graphic%20Rendition)%20parameters-,Colors,-3%2Dbit%20and
+
+package ansi
+
+import "fmt"
+
+func Red(s string) string {
+	return fmt.Sprintf("\u001b[31m%s\u001b[0m", s)
+}
+
+func RedBG(s string) string {
+	return fmt.Sprintf("\u001b[41m%s\u001b[0m", s)
+}
+
+func Green(s string) string {
+	return fmt.Sprintf("\u001b[32m%s\u001b[0m", s)
+}
+
+func GreenBG(s string) string {
+	return fmt.Sprintf("\u001b[42m%s\u001b[0m", s)
+}
+
+func YellowBG(s string) string {
+	return fmt.Sprintf("\u001b[43m%s\u001b[0m", s)
+}
+
+func WhiteBG(s string) string {
+	return fmt.Sprintf("\u001b[47m%s\u001b[0m", s)
+}
+
+func Underline(s string) string {
+	return fmt.Sprintf("\u001b[4m%s\u001b[0m", s)
+}
